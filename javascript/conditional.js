b09b81c86905a594ac9b32f7fa60400e9775e761
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-07 21:55:54 -0500

    conditionals are working, update doc block

---
+++ javascript/conditional.js
@@ -0,0 +1,16 @@
+/**conditionals exercise from Udemy
+ * A conditional is put inside ()
+ * a conditional is usually a comparison , a way to evaluate if something is true
+ * 
+ */
+
+ let num1 = 45
+ let num2 = 45
+
+ if(num1 < num2){
+     console.log("number 1 is less than 2");
+ }else if (num1 > num2){
+     console.log("number 1 is not less than number 2");
+ }else{
+     console.log("The numbers are the same");
+ }
