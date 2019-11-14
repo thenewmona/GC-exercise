9cba8b64865c7f7f091df01c99f2e46bd9d5b423
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-14 18:34:49 -0500

    work with Chris
    working with Chris on functions

--- exercise 3/time.js
+++ exercise 3/time.js
@@ -6,11 +6,30 @@
 // mins(4);
 
 // declaration - parameter
-function hourToMin (hour){
-    let min = 60;//statement
+//declarative
+// function hourToMin (hour){
+//     let min = 60;//statement
     
-    return (hour*min);//invoking the function
-}
-//calling the function 
-hour(6);
+//     return (hour*min);//invoking the function
+// }
+// //calling the function 
+// hourToMin(6);
+// console.log(hourToMin(6));
 
+//declaring a variable into a function
+// let hourToMin = function(hour) {
+// let min =60;
+// return (hour*min);
+// }
+// console.log (hourToMin(7));
+
+//arrow
+// declaring the variable with a arrow function, fat arrow just gets rid of the word function
+let hourToMin = () =>{
+    let min = 1;
+    let sec = 60;
+    return (sec*min);
+
+}
+//calling the function
+console.log(hourToMin());
