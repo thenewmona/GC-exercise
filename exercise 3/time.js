c0d8a5a2577b2612bac9ec0728ab1318af15e711
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-23 14:01:35 -0500

    reset time.js

--- exercise 3/time.js
+++ exercise 3/time.js
@@ -1,41 +1,35 @@
-// "use strict"
-// // function minToSecs (mins){
-// //     let sec =60; 
-// //     return (mins*sec);    
-// // }
-// // mins(4);
-
-// // declaration - parameter
-// //declarative
-// // function hourToMin (hour){
-// //     let min = 60;//statement
-    
-// //     return (hour*min);//invoking the function
-// // }
-// // //calling the function 
-// // hourToMin(6);
-// // console.log(hourToMin(6));
-
-// //declaring a variable into a function
-// // let hourToMin = function(hour) {
-// // let min =60;
-// // return (hour*min);
-// // }
-// // console.log (hourToMin(7));
-
-// //arrow
-// // declaring the variable with a arrow function, fat arrow just gets rid of the word function
-// let hourToMin = () =>{
-//     let min = 1;
-//     let sec = 60;
-//     return (sec*min);
-
-
+"use strict"
+// function minToSecs (mins){
+//     let sec =60; 
+//     return (mins*sec);    
 // }
-// //calling the function
-// console.log(hourToMin());
+// mins(4);
 
-function sayHello (name){
-    console.log('Ramona');
+// declaration - parameter
+//declarative
+// function hourToMin (hour){
+//     let min = 60;//statement
+    
+//     return (hour*min);//invoking the function
+// }
+// //calling the function 
+// hourToMin(6);
+// console.log(hourToMin(6));
+
+//declaring a variable into a function
+// let hourToMin = function(hour) {
+// let min =60;
+// return (hour*min);
+// }
+// console.log (hourToMin(7));
+
+//arrow
+// declaring the variable with a arrow function, fat arrow just gets rid of the word function
+let hourToMin = () =>{
+    let min = 1;
+    let sec = 60;
+    return (sec*min);
+
 }
-sayHello()
+//calling the function
+console.log(hourToMin());
