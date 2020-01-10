3418e13d3406b6e7470f2e6e9417fed751a83673
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-09 21:16:23 -0500

    I am so lost with this

--- angular/mad-lib/src/app/app.component.ts
+++ angular/mad-lib/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-first-angular-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mad-lib';
+  hideTitle = true;
+
+  returnTitle() {
+    return this.title;
+  }
+
+  toggleTitle($event) {
+    console.log($event)
+    this.hideTitle = !this.hideTitle;
+  }
 }
