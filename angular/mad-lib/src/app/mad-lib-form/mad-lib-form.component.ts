0c578666d74ec22d88a49a4748f0c2dbca559566
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-09 21:20:12 -0500

    created madlib form with ng gen comp

---
+++ angular/mad-lib/src/app/mad-lib-form/mad-lib-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mad-lib-form',
+  templateUrl: './mad-lib-form.component.html',
+  styleUrls: ['./mad-lib-form.component.css']
+})
+export class MadLibFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
