df62968ffaea5021149648320a7f3fb8cef89106
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-15 20:54:43 -0500

    created updown component

--- angular/GC-exercise-3/updowncounter/src/app/app.module.ts
+++ angular/GC-exercise-3/updowncounter/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpDownCounterComponent } from './up-down-counter/up-down-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpDownCounterComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/GC-exercise-3/updowncounter/src/app/up-down-counter/up-down-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-up-down-counter',
+  templateUrl: './up-down-counter.component.html',
+  styleUrls: ['./up-down-counter.component.css']
+})
+export class UpDownCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
