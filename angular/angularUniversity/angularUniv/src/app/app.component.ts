6e14093cec992da470a0d742ef788b23fe1334d0
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-16 21:45:07 -0500

    added image with a click, to alert

--- angular/angularUniversity/angularUniv/src/app/app.component.ts
+++ angular/angularUniversity/angularUniv/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Monas AngularUniv Project';
+  data = {
+    title:'Monas angular tutorial'
+  };
+  onLogoClicked(){
+    alert('hello World')
+  }
 }
